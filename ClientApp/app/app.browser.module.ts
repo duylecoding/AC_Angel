1bf2f43d21c451f93c3a4469c6210767a0494216
Duy Le <duylezero@gmail.com>
2019-01-26 14:00:00 -0600

    dashboard

--- ClientApp/app/app.browser.module.ts
+++ ClientApp/app/app.browser.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppModuleShared } from './app.shared.module';
 import { AppComponent } from './components/app/app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     bootstrap: [ AppComponent ],
